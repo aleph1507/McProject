0fb790aeea351c68ed0665a69de4dc5c67bb644d
hristo <xrristo@gmail.com>
2019-08-07 17:58:27 +0200

    safari0001

--- partials/_render_video.php
+++ partials/_render_video.php
@@ -4,7 +4,7 @@
         <br> File webViewLink: <?= $file->webViewLink; ?>
         <br> Thumbnail: <?= $file->thumbnailLink ?>
     </div>
-    <div class="preview" data-src="<?= str_replace('/view', '/preview', $file->webviewLink); ?>">
+    <div class="preview" data-src="<?= str_replace('/view', '/preview', $file->webViewLink); ?>">
 <!--        <video width="40%" src="< $file->webContentLink; ?>" controls></video>-->
 <!--        <iframe target="_parent" class="video-embed" data-src="<//= $file->webViewLink; ?>" src="<//= str_replace('/view', '/preview', $file->webViewLink); ?>" frameborder="0"></iframe>-->
 	<!--<iframe src="<= $file->webViewLink; ?>" frameborder="0"></iframe> -->
